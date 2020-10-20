f28e8c03485b4b8a99ead07117d3b59a4ecfec8b
Jasom Park <zeipar@gmail.com>
2020-10-20 12:52:23 -0700

    Fixed missing import file name.

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "";
+import Home from "../components/Home";
 export default function AppRouter() {
   return (
     <Router>
