5cde3d22dca5a6f9475e85e9b54704247e49853d
Jasom Park <zeipar@gmail.com>
2020-10-20 12:51:31 -0700

    Set path to Home in the router.

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "";
 export default function AppRouter() {
   return (
     <Router>
