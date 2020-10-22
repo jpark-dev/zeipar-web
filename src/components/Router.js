b1adb4a07ec7b929d55c7efff4f5fe3da2b47d7b
Jasom Park <zeipar@gmail.com>
2020-10-21 23:51:37 -0700

    Added home and portfolio pages.

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "../components/Home";
+import Home from "../routes/Home";
+import Portfolio from "../routes/Portfolio";
 export default function AppRouter() {
   return (
     <Router>
