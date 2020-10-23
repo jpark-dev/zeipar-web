fcf1ecfe39034d1988add0fffed6a95e3df30a86
Jasom Park <zeipar@gmail.com>
2020-10-22 23:44:10 -0700

    Created css for home layout section boxes and imported it.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import "../styles/main_layout.css";
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      <div>Home</div>
+      <section className="section">Latest Updates</section>
+      <section className="section">Try out Jason's applications</section>
     </>
   );
 }
