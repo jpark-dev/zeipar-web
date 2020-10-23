192f35fd2eac33700a6fd5628f9a5a5db4f3a605
Jasom Park <zeipar@gmail.com>
2020-10-22 23:36:52 -0700

    Created a NavBar component and imported it.

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/nav.css";
+
+export default function NavBar() {
+  return (
+    <>
+      <div class="nav-wrapper">
+        <div>Logo</div>
+        <div>Profile</div>
+        <div>Portfolio</div>
+        <div>Translations</div>
+        <div>Others</div>
+      </div>
+    </>
+  );
+}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,11 @@
 import React from "react";
+import NavBar from "../components/NavBar";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <NavBar />
+      <div>Home</div>
+    </>
+  );
 }
